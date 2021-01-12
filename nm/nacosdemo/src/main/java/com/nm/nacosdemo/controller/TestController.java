e18fb39dd22183c8a52a0454e87a9262796b5863
BaoStorm <609311029@qq.com>
2021-01-13 00:15:11 +0800

    feat : sentinel

--- nm/nacosdemo/src/main/java/com/nm/nacosdemo/controller/TestController.java
+++ nm/nacosdemo/src/main/java/com/nm/nacosdemo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.nm.nacosdemo.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.nm.nmcommon.model.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/test/get")
-    public Result<String > get(){
+    @SentinelResource(value = "getByCode",blockHandler = "handleException")
+    public Result<String > get() {
         return Result.succeed("Hello Nacos Demo");
+    }
+
+    /**
+     * 自定义异常策略
+     * 返回值和参数要跟目标函数一样，参数可以追加BlockException
+     */
+    public Result<String> handleException(BlockException exception){
+//        log.info("flow exception{}",exception.getClass().getCanonicalName());
+        return Result.failed("达到阈值了,不要再访问了!");
     }
 }
